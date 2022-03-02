cedb4ca7ffe7ce0866616b0855400509833d9164
Jake <joregister91@gmail.com>
2022-03-02 18:54:55 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function myFunction() {
+  var menuLinks = document.getElementById("burgerLinks");
+  if (menuLinks.style.display === "block") {
+    menuLinks.style.display = "none";
+  } else {
+    menuLinks.style.display = "block";
+  }
+
+  var icons = document.getElementById("icons");
+  if (icons.classList.contains("fa-bars")) {
+    icons.classList.remove("fa-bars");
+    icons.classList.add("fa-xmark");
+  } else {
+    icons.classList.remove("fa-xmark");
+    icons.classList.add("fa-bars");
+  }
+}
