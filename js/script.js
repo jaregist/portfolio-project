75cc32031432720a57ee77c21b02ccfb53604ceb
Jake <joregister91@gmail.com>
2022-04-10 16:48:34 -0400

    scroll to top button and function

--- js/script.js
+++ js/script.js
@@ -21,3 +21,22 @@ function myFunction() {
     icons.classList.add("fa-bars");
   };
 }
+
+//Scroll to top code
+mybutton = document.getElementById("top-btn");
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; //Safari
+  document.documentElement.scrollTop = 0; //Chrome, Firefox, IE and Opera
+}
